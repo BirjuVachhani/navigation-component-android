cc4605fba216acc8c79636fc88f124cbb7144b53
Birju Vachhani <brvachhani@gmail.com>
2018-07-03 01:27:45 +0530

    style: add drawer and option menu

--- app/src/main/java/com/birjuvachhani/navigationcomponentdemo/MainActivity.java
+++ app/src/main/java/com/birjuvachhani/navigationcomponentdemo/MainActivity.java
@@ -1,13 +1,44 @@
 package com.birjuvachhani.navigationcomponentdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolBar;
+    private NavigationView nvMain;
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolBar = findViewById(R.id.toolBar);
+        nvMain = findViewById(R.id.nvMain);
+        mDrawerLayout = findViewById(R.id.drawerLayout);
+        setSupportActionBar(mToolBar);
+        setDrawer();
+    }
+
+    private void setDrawer() {
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolBar, R.string.open, R.string.close);
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
